a145f530f011c426b0c46ab757c7a997eb5cc9d7
brnbruno <b.h.oliveira7@gmail.com>
2017-01-03 12:19:46 -0200

    0.1

---
+++ EditorSimplesHTML/EditorSimplesHTML/MainForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Valdelis
+ * Data: 03/01/2017
+ * Hora: 07:39
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+namespace EditorSimplesHTML
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.fontDialog1 = new System.Windows.Forms.FontDialog();
+			this.button1 = new System.Windows.Forms.Button();
+			this.toolStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(292, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.BackColor = System.Drawing.Color.Transparent;
+			this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.toolStripButton1,
+									this.toolStripButton2,
+									this.toolStripButton4,
+									this.toolStripButton3});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(292, 25);
+			this.toolStrip1.TabIndex = 1;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton1.Text = "toolStripButton1";
+			this.toolStripButton1.Click += new System.EventHandler(this.ToolStripButton1Click);
+			// 
+			// toolStripButton2
+			// 
+			this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+			this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton2.Name = "toolStripButton2";
+			this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton2.Text = "toolStripButton2";
+			this.toolStripButton2.Click += new System.EventHandler(this.ToolStripButton2Click);
+			// 
+			// toolStripButton4
+			// 
+			this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+			this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton4.Name = "toolStripButton4";
+			this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton4.Text = "toolStripButton4";
+			this.toolStripButton4.Click += new System.EventHandler(this.ToolStripButton4Click);
+			// 
+			// toolStripButton3
+			// 
+			this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+			this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton3.Name = "toolStripButton3";
+			this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton3.Text = "toolStripButton3";
+			this.toolStripButton3.Click += new System.EventHandler(this.ToolStripButton3Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 49);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.tabControl1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+			this.splitContainer1.Size = new System.Drawing.Size(292, 224);
+			this.splitContainer1.SplitterDistance = 145;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(145, 224);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.richTextBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(137, 198);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(131, 192);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1TextChanged);
+			this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RichTextBox1KeyPress);
+			this.richTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.RichTextBox1KeyUp);
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.richTextBox2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(137, 198);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox2.Location = new System.Drawing.Point(3, 3);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(131, 192);
+			this.richTextBox2.TabIndex = 0;
+			this.richTextBox2.Text = "";
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(143, 224);
+			this.webBrowser1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(170, 24);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(292, 273);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.toolStrip1);
+			this.Controls.Add(this.menuStrip1);
+			this.KeyPreview = true;
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "EditorSimplesHTML";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ToolStripButton toolStripButton4;
+		private System.Windows.Forms.FontDialog fontDialog1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.ToolStripButton toolStripButton3;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.WebBrowser webBrowser1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.ToolStripButton toolStripButton2;
+		private System.Windows.Forms.ToolStripButton toolStripButton1;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+	}
+}
---
+++ EditorSimplesHTML/EditorSimplesHTML/MainForm.cs
@@ -0,0 +1,126 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Valdelis
+ * Data: 03/01/2017
+ * Hora: 07:39
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EditorSimplesHTML
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void ToolStripButton2Click(object sender, EventArgs e)
+		{
+			if (splitContainer1.Orientation == Orientation.Horizontal) {
+				splitContainer1.Orientation = Orientation.Vertical;
+			}else
+			{
+				splitContainer1.Orientation = Orientation.Horizontal;
+			}
+		}
+		
+		void RichTextBox1KeyUp(object sender, KeyEventArgs e)
+		{
+			webBrowser1.DocumentText = richTextBox1.Text;
+			if(e.KeyValue == 190){
+				Class1.AddColouredText(richTextBox1.Text,richTextBox1);
+				
+			}			
+			
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void ToolStripButton3Click(object sender, EventArgs e)
+		{
+			if (fontDialog1.ShowDialog() == DialogResult.OK)
+        	{
+            	//if (richTextBox1.SelectedText != "") Texto Selecionado
+            	//{
+					richTextBox1.Font = fontDialog1.Font;
+            	//}
+			}
+		}
+		
+		void ToolStripButton1Click(object sender, EventArgs e)
+		{
+			// Create a SaveFileDialog to request a path and file name to save to.
+   		SaveFileDialog saveFile1 = new SaveFileDialog();
+
+   // Initialize the SaveFileDialog to specify the RTF extension for the file.
+   		saveFile1.DefaultExt = "*.html";
+   		saveFile1.Filter = "HTML Files|*.html";
+
+   // Determine if the user selected a file name from the saveFileDialog.
+  		if(saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+   		   saveFile1.FileName.Length > 0) 
+   		{
+      // Save the contents of the RichTextBox into the file.
+     	 richTextBox1.SaveFile(saveFile1.FileName, RichTextBoxStreamType.PlainText);
+  		 }
+		}
+		
+		
+		
+		void ToolStripButton4Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFile1 = new OpenFileDialog();
+
+   			// Initialize the OpenFileDialog to look for RTF files.
+   			openFile1.DefaultExt = "*.html";
+   			openFile1.Filter = "HTML Files|*.html";
+
+   			// Determine whether the user selected a file from the OpenFileDialog.
+  			if(openFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+      		openFile1.FileName.Length > 0) 
+   			{
+     		// Load the contents of the file into the RichTextBox.
+     		richTextBox1.LoadFile(openFile1.FileName,RichTextBoxStreamType.PlainText);
+     		webBrowser1.DocumentText = richTextBox1.Text;
+     		Class1.AddColouredText(richTextBox1.Text,richTextBox1);
+   			}
+		}
+		
+		void RichTextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+			
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void RichTextBox1KeyPress(object sender, KeyPressEventArgs e)
+		{
+			//if (char.IsWhiteSpace(e.KeyChar)) {
+			
+			
+		}
+	}
+}
